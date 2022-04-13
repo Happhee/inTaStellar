6688d5ab6c33348c4268187650d95bec15d78b31
seohee0112 <ghdtj7@naver.com>
2022-04-13 20:15:30 +0900

    chore : ⚙️ 선택적 매개변수 오류 코드 및 해결 코드 추가

--- week1/function.ts
+++ week1/function.ts
@@ -10,3 +10,12 @@ console.log(profile("seohee"));
 
 // console.log(profile("seohee", 24, 25));
 //1-2개의 인수가 필요한데 3개를 가져왔습니다.ts(2554)
+
+//필수 매개 변수는 선택적 매개 변수 뒤에 올 수 없습니다.ts(1016)
+// function changeProfile(name?: string, age: number): string {
+//   return `My name is ${name},  I am ${age} years old.`;
+// }
+function changeProfile(name: string | undefined, age: number): string {
+  return `My name is ${name},  I am ${age} years old.`;
+}
+console.log(changeProfile(undefined, 24));
